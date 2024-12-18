a22f4b0fed5b745f3edca7a5a6c9d0b82c8bb250
Tigazin <tigaz1@bk.ru>
2024-12-18 08:55:12 +0300

    Добавьте файлы проекта.

---
+++ app/models.py
@@ -0,0 +1,66 @@
+﻿"""
+Definition of models.
+"""
+
+from django.db import models
+from django.contrib import admin
+from datetime import datetime 
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+class Blog(models.Model):
+
+    title = models.CharField(max_length = 100, unique_for_date = "posted", verbose_name = "Заголовок")
+
+    description = models.TextField(verbose_name = "Краткое содержание")
+
+    content = models.TextField(verbose_name = "Полное содержание")
+
+    posted = models.DateTimeField(default = datetime.now(), db_index = True, verbose_name = "Опубликована")
+
+    author = models.ForeignKey(User, null=True, blank=True, on_delete = models.SET_NULL, verbose_name = "Автор")
+    image = models.FileField(default = 'temp.jpg',verbose_name = 'Путь к картинке')
+
+# Методы класса:
+
+def get_absolute_url(self): # метод возвращает строку с URL-адресом записи
+
+    return reverse("blogpost", args=[str(self.id)])
+
+def __str__(self): # метод возвращает название, используемое для представления отдельных записей в административном разделе
+
+    return self.title
+
+# Метаданные – вложенный класс, который задает дополнительные параметры модели:
+
+class Meta:
+
+    db_table = "Posts" # имя таблицы для модели
+
+    ordering = ["-posted"] # порядок сортировки данных в модели ("-" означает по убыванию)
+
+    verbose_name = "статья блога" # имя, под которым модель будет отображаться в административном разделе (для одной статьи блога)
+
+    verbose_name_plural = "статьи блога" # тоже для всех статей блога
+
+admin.site.register(Blog)
+class Comment(models.Model):
+    text = models.TextField(verbose_name="Текст комментария")
+    date = models.DateTimeField(default=datetime.now(), db_index=True, verbose_name="Дата комментария")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Автор комментария")
+    post = models.ForeignKey(Blog, on_delete=models.CASCADE, verbose_name="Статья комментария")
+   
+
+    # Методы класса:
+    def __str__(self):  # метод возвращает название, используемое для представления отдельных записей в административном разделе
+        return 'Комментарий %d из %s к %s' % (self.id, self.author, self.post)
+
+    # Метаданные — вложенный класс, который задает дополнительные параметры модели:
+    class Meta:
+        db_table = "Comment"
+        ordering = ["-date"]
+        verbose_name = "Комментарий к статье блога"
+        verbose_name_plural = "Комментарии к статьям блога"
+
+admin.site.register(Comment)
+# Create your models here.
---
+++ app/views.py
@@ -0,0 +1,201 @@
+﻿"""
+Definition of views.
+"""
+
+from datetime import datetime
+from django.http.response import HttpResponse
+from django.shortcuts import redirect, render
+from django.http import HttpRequest
+from .forms import FeedbackForm
+from django.contrib.auth.forms import UserCreationForm
+from django.db import models
+from .models import Blog
+from .models import Comment # использование модели комментариев
+
+from .forms import CommentForm # использование формы ввода комментария
+from .forms import BlogForm # использование формы ввода комментария
+
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/index.html',
+        {
+            'title':'Главная',
+            'year':datetime.now().year,
+        }
+    )
+
+def blog(request):
+    posts = Blog.objects.all()
+
+    assert isinstance(request,HttpRequest)
+    return render(
+        request,
+        'app/blog.html',{
+        'title':'Блог',
+        'posts':posts,
+        'year':datetime.now().year,
+        })
+
+def blogpost(request,parametr):
+    assert isinstance(request,HttpRequest)
+    post_1 = Blog.objects.get(id=parametr)
+    comments = Comment.objects.filter(post=parametr)
+    if request.method == "POST": # после отправки данных формы на сервер методом POST
+
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+
+            comment_f = form.save(commit=False)
+
+            comment_f.author = request.user # добавляем (так как этого поля нет в форме) в модель Комментария (Comment) в поле автор авторизованного пользователя
+
+            comment_f.date = datetime.now() # добавляем в модель Комментария (Comment) текущую дату
+
+            comment_f.post = Blog.objects.get(id=parametr) # добавляем в модель Комментария (Comment) статью, для которой данный комментарий
+
+            comment_f.save() # сохраняем изменения после добавления полей
+
+        return redirect('blogpost', parametr=post_1.id) # переадресация на ту же страницу статьи после отправки комментария
+
+    else:
+
+        form = CommentForm() # создание формы для ввода комментария
+    return render(
+        request,
+        'app/blogpost.html',{
+            'post_1':post_1,
+            'year':datetime.now().year,
+            'comments': comments, # передача всех комментариев к данной статье в шаблон веб-страницы
+
+            'form': form, 
+            }
+        )
+def contact(request):
+    """Renders the contact page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/contact.html',
+        {
+            'title':'Контакты',
+            'message':'Тут контакты',
+            'year':datetime.now().year,
+        }
+    )
+
+def about(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/about.html',
+        {
+            'title':'О нас',
+            'message':'Тут о нас',
+            'year':datetime.now().year,
+        }
+    )
+def links(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/links.html',
+        {
+
+        }
+    )
+def videopost(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/videopost.html',
+        {
+
+        }
+    )
+def feedback(request):
+    assert isinstance(request, HttpRequest)
+    data = None
+
+    if request.method == 'POST':
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            data = {
+                'name': form.cleaned_data['name'],
+                'city': form.cleaned_data['city'],
+                'product': form.cleaned_data['product'],
+                'rating': form.cleaned_data['rating'],
+                'usage': form.cleaned_data['usage'],
+                'subscribe': 'Да' if form.cleaned_data['subscribe'] else 'Нет',
+                'email': form.cleaned_data['email'],
+                'message': form.cleaned_data['message'],
+            }
+            form = None
+        else:
+            data = None
+    else:
+        form = FeedbackForm()
+
+    return render(
+        request,
+        'app/feedback.html',
+        {
+            'form': form,
+            'data': data,
+        }
+    )
+def newpost(request):
+
+    if request.method == 'POST':
+        blogform = BlogForm(request.POST, request.FILES)
+        if blogform.is_valid():
+            blog_f = blogform.save(commit=False)
+            blog_f.posted = datetime.now()
+            blog_f.autor = request.user
+            blog_f.save()
+            return redirect('blog')
+    else:
+        blogform = BlogForm()
+    
+    return render(
+        request,
+        'app/newpost.html',
+        {
+            'blogform': blogform,
+            'title': 'Добавить статью блога',
+            'year': datetime.now().year,
+        }
+    )
+def registration(request):
+    if request.method =="POST":
+        regform = UserCreationForm(request.POST)
+        if regform.is_valid():
+            reg_f = regform.save(commit=False)
+            reg_f.is_staff = False
+            reg_f.is_active = True
+            reg_f.is_superuser = False
+            reg_f.date_joined = datetime.now()
+            reg_f.last_login = datetime.now()
+
+            regform.save()
+
+            return redirect('home')
+    else:
+         regform = UserCreationForm()
+    assert isinstance(request,HttpRequest)
+    return render(
+        request,
+        'app/registration.html',
+        {
+            'regform':regform,
+            'year':datetime.now().year,
+        }
+        )
+     
